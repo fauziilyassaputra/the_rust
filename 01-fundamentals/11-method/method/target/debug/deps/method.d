8e38eecb807d160f168234e7ffba50c15af37ec0
fauziilyassaputra <fauziilyassaputra@gmail.com>
2026-05-16 19:57:39 +0700

    create method

---
+++ 01-fundamentals/11-method/method/target/debug/deps/method.d
@@ -0,0 +1,5 @@
+D:\koding\rust\01-fundamentals\11-method\method\target\debug\deps\method.d: src\main.rs
+
+D:\koding\rust\01-fundamentals\11-method\method\target\debug\deps\method.exe: src\main.rs
+
+src\main.rs:
---
+++ 01-fundamentals/11-method/method/target/debug/method.d
@@ -0,0 +1,1 @@
+D:\koding\rust\01-fundamentals\11-method\method\target\debug\method.exe: D:\koding\rust\01-fundamentals\11-method\method\src\main.rs
